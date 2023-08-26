f713af5688dde59b80143daebad8a698c9e2d5b7
runeblaze <runeblaze@excite.co.jp>
2023-08-25 23:31:02 -0700

    remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod exposure;
 mod lca;
 pub use lca::*;
-use ogcat::ogtree::TreeCollection;
+
 
 // this is the driver code for testing during development
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
